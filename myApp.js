4dfeefa1bab5fb3c6e04c47bacc4c57344ad7809
mynakturk <mynakturk@gmail.com>
2019-08-30 22:03:18 +0300

    edit

--- myApp.js
+++ myApp.js
@@ -23,7 +23,11 @@ app.use(express.static(__dirname + "/public"));
 
 /** 5) serve JSON on a specific route */
 app.get("/json", function (req, res) {
-    res.json({"message": "Hello json"});
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        app.get("/json", (req, res) => res.json({"message": "HELLO JSON"}));
+    } else {
+        app.get("/json", (req, res) => res.json({"message": "Hello json"}));
+    }
 });
 
 
